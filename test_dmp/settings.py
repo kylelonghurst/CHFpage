ce743cd10ad4854f1fd40cc98dfd41423330f88b
Kyle Longhurst <kylelonghurst@gmail.com>
2015-01-30 16:05:03 -0700

    Added Models.

--- test_dmp/settings.py
+++ test_dmp/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django_mako_plus.controller',
     'homepage',
+    'polymorphic',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -86,8 +87,12 @@ WSGI_APPLICATION = 'test_dmp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'CHF',
+        'USER': 'kylelonghurst',
+        'PASSWORD': 'hAppy2',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
